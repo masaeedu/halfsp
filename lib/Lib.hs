576f2bf45fcab7aa0f5ea020dfc11fc212f5966e
Asad Saeeduddin <masaeedu@gmail.com>
2021-11-07 15:26:31 -0400

    Extend module lookup hack

--- lib/Lib.hs
+++ lib/Lib.hs
@@ -1,18 +1,28 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Lib (serverMain) where
 
+import Control.Applicative
 import Control.Arrow ((&&&))
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Trans.Maybe (runMaybeT)
 import Data.Bifunctor (Bifunctor (first, second))
+import Data.Coerce
+import Data.Foldable (asum)
+import Data.Functor ((<&>))
+import Data.Functor.Compose
+import Data.Kind
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (listToMaybe)
+import Data.Monoid (Endo (..))
+import Data.String.Conversions
 import Data.Text (Text, intercalate, pack, replace, unpack)
 import DynFlags (DynFlags)
 import GhcideSteal (gotoDefinition, hoverInfo, symbolKindOfOccName)
@@ -62,11 +72,12 @@ import Language.LSP.Types
     SMethod (STextDocumentDefinition, STextDocumentHover, SWorkspaceSymbol),
     SymbolInformation (..),
     TextDocumentIdentifier (..),
+    Uri,
     WorkspaceSymbolParams (WorkspaceSymbolParams, _query),
     filePathToUri,
     sectionSeparator,
     uriToFilePath,
-    type (|?) (InL, InR), Uri
+    type (|?) (InL, InR),
   )
 import Language.LSP.Types.Lens (uri)
 import Lens.Micro ((^.))
@@ -74,6 +85,7 @@ import Module (ModuleName, moduleNameSlashes, moduleNameString)
 import OccName
   ( occNameString,
   )
+import System.Directory (doesFileExist)
 import System.FilePath ((<.>), (</>))
 import Utils (EK, ekbind, eklift, etbind, etpure, fromJust, kbind, kcodensity, kliftIO)
 
@@ -106,14 +118,43 @@ coordsHieDbToLSP (l, c) = Position {_line = l - 1, _character = c - 1}
 coordsLSPToHieDb :: Position -> (Int, Int)
 coordsLSPToHieDb Position {..} = (_line + 1, _character + 1)
 
+-- {{{ hacky garbage that needs to be replaced
+
+hardcodedSourceDirs :: [Text]
+hardcodedSourceDirs = ["src", "test"]
+
+replaceMany :: [Text] -> Text -> Text -> Text
+replaceMany patterns substitution = appEndo . foldMap (Endo . replace substitution) $ patterns
+
+-- Less dumb alternative compose
+type LDAC :: (Type -> Type) -> (Type -> Type) -> Type -> Type
+newtype LDAC f g a = LDAC (f (g a))
+  deriving (Functor)
+  deriving (Applicative) via (Compose f g)
+
+instance (Applicative f, Alternative g) => Alternative (LDAC f g) where
+  empty = LDAC $ pure empty
+  LDAC x <|> LDAC y = LDAC $ liftA2 (<|>) x y
+
+whicheverOfManyThingsWorks :: [a -> IO (Maybe b)] -> a -> IO (Maybe b)
+whicheverOfManyThingsWorks = coerce . asum . fmap (LDAC . LDAC)
+
 -- TODO: Make this less hacky, involves fixing up the NULL entries in the modules table in hiedb
 textDocumentIdentifierToHieFilePath :: TextDocumentIdentifier -> HieTarget
 textDocumentIdentifierToHieFilePath (TextDocumentIdentifier u) =
-  Left $ unpack $ replace ".hs" ".hie" $ replace "src" ".hiefiles" $ pack $ fromJust $ uriToFilePath u
+  Left $ unpack $ replace ".hs" ".hie" $ replace "test" ".hiefiles" $ replaceMany hardcodedSourceDirs ".hiefiles" $ pack $ fromJust $ uriToFilePath u
 
 -- TODO: Get rid of this hack, defer to populated modules table
-moduleToTextDocumentIdentifier :: FilePath -> ModuleInfo -> TextDocumentIdentifier
-moduleToTextDocumentIdentifier wsroot = TextDocumentIdentifier . filePathToUri . (wsroot </>) . ("src" </>) . (<.> "hs") . moduleNameSlashes . modInfoName
+moduleToTextDocumentIdentifier :: FilePath -> ModuleInfo -> IO (Maybe TextDocumentIdentifier)
+moduleToTextDocumentIdentifier wsroot = (fmap . fmap) (TextDocumentIdentifier . filePathToUri) . whicheverOfManyThingsWorks (fmap (moduleFileInSourceDirIfExists . cs) hardcodedSourceDirs)
+  where
+    moduleFileInSourceDirIfExists :: FilePath -> ModuleInfo -> IO (Maybe FilePath)
+    moduleFileInSourceDirIfExists sourceDir = ensureFilePathExists . (wsroot </>) . (sourceDir </>) . (<.> "hs") . moduleNameSlashes . modInfoName
+
+    ensureFilePathExists :: FilePath -> IO (Maybe FilePath)
+    ensureFilePathExists path = doesFileExist path <&> \b -> if b then Just path else Nothing
+
+-- }}}
 
 astsAtPoint :: HieFile -> (Int, Int) -> Maybe (Int, Int) -> [HieAST TypeIndex]
 astsAtPoint hiefile start end = pointCommand hiefile start end id
@@ -143,36 +184,42 @@ hiedbErrorToResponseError = flip (ResponseError InternalError) Nothing . renderH
 moduleSourcePathMap :: HieDb -> FilePath -> IO (Map ModuleName Uri)
 moduleSourcePathMap hiedb wsroot = do
   rows <- getAllIndexedMods hiedb
-  pure $ fmap (^. uri) $ M.fromList $ fmap ((modInfoName &&& moduleToTextDocumentIdentifier wsroot) . hieModInfo) rows
+  m <- sequenceA $ M.fromList $ fmap ((modInfoName &&& moduleToTextDocumentIdentifier wsroot) . hieModInfo) rows
+  pure $ (M.mapMaybe . fmap) (^. uri) $ m
 
 -- The actual code
 
-symbolInfo :: FilePath -> Res DefRow -> SymbolInformation
-symbolInfo wsroot (DefRow {..} :. mod) =
-  SymbolInformation
-    { _name = pack $ occNameString defNameOcc,
-      _kind = symbolKindOfOccName defNameOcc,
-      _deprecated = Nothing,
-      _location =
-        Location
-          { _uri = moduleToTextDocumentIdentifier wsroot mod ^. uri,
-            _range =
-              Range
-                { _start = coordsHieDbToLSP (defSLine, defSCol),
-                  _end = coordsHieDbToLSP (defELine, defECol)
-                }
-          },
-      _containerName = Just $ pack $ moduleNameString $ modInfoName mod
-    }
+symbolInfo :: FilePath -> Res DefRow -> IO SymbolInformation
+symbolInfo wsroot (DefRow {..} :. m) = do
+  mtdi <- moduleToTextDocumentIdentifier wsroot m
+  case mtdi of
+    Nothing -> fail "unable to find text document corresponding to module"
+    Just tdi ->
+      pure $
+        SymbolInformation
+          { _name = pack $ occNameString defNameOcc,
+            _kind = symbolKindOfOccName defNameOcc,
+            _deprecated = Nothing,
+            _location =
+              Location
+                { _uri = tdi ^. uri,
+                  _range =
+                    Range
+                      { _start = coordsHieDbToLSP (defSLine, defSCol),
+                        _end = coordsHieDbToLSP (defELine, defECol)
+                      }
+                },
+            _containerName = Just $ pack $ moduleNameString $ modInfoName m
+          }
 
 handleWorkspaceSymbolRequest :: Handlers (LspT c IO)
 handleWorkspaceSymbolRequest = requestHandler SWorkspaceSymbol $ \req ->
   getWsRoot `ekbind` \wsroot -> eklift . kcodensity . liftIO $ do
     results <- withHieDb (wsroot </> ".hiedb") $ flip searchDef $ unpack $ requestQuery req
-    pure $ prepareSymbolResults wsroot results
+    prepareSymbolResults wsroot results
   where
-    prepareSymbolResults :: FilePath -> [Res DefRow] -> List SymbolInformation
-    prepareSymbolResults wsroot = List . fmap (symbolInfo wsroot)
+    prepareSymbolResults :: FilePath -> [Res DefRow] -> IO (List SymbolInformation)
+    prepareSymbolResults wsroot = fmap List . traverse (symbolInfo wsroot)
 
     requestQuery :: Message 'WorkspaceSymbol -> Text
     requestQuery RequestMessage {_params = WorkspaceSymbolParams {_query = q}} = q
